5bfbcca9dcc52b22d882d70f2164a55dc7070ebb
IslandofDream <rhffldkt0517@naver.com>
2022-07-26 16:25:48 +0900

    [Bronze V] Title: 아!, Time: 120 ms, Memory: 13228 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+        val me_ah = nextLine()
+        val doc_ah = nextLine()
+    if(me_ah.length >= doc_ah.length) print("go")
+    else print("no")
+}
