b798f30713b6c753e6a8d91ac707e9d5d22f72ad
IslandofDream <rhffldkt0517@naver.com>
2022-07-30 16:41:02 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 128 ms, Memory: 13244 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+    fun main()= with(Scanner(System.`in`)) {
+        print(pivo(nextInt()))
+    }
+
+    fun pivo(num: Int): Int {
+        if (num == 0)
+            return 0
+        else if (num == 1)
+            return 1
+        if (num >= 2) {
+            return pivo(num - 1) + pivo(num - 2)
+        }
+        return -1
+    }
