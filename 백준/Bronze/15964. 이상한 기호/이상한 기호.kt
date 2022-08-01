9584c9814e38d1d09dad9ecd4c62c246b0ff9dbe
IslandofDream <rhffldkt0517@naver.com>
2022-08-01 14:37:14 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.kt
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.kt
@@ -2,6 +2,6 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val array = readLine().split(" ").map { it.toLong()}
-    print((array[0]+array[1])*(array[0]-array[1]))
+    val (a,b) = readLine().split(" ").map { it.toLong()}
+    print((a+b)*(a-b))
 }
