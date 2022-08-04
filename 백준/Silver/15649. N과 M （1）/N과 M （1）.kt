9d195cc8bae359246084c4010c474915da862621
IslandofDream <rhffldkt0517@naver.com>
2022-08-04 20:16:45 +0900

    Update N과 M （1）.kt

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -1,26 +1,23 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
-fun main() =with(BufferedReader(InputStreamReader(System.`in`))){
-    val (n,m)= readLine().split(" ").map { it.toInt() }
-    val num = IntArray(m){ 0 }
-    val used = BooleanArray(n + 1) { false }
+fun main(){
+    val (n,m) = readLine()!!.split(" ").map{it.toInt()}
+    val num = IntArray(m) {0}
+    val used = BooleanArray(n+1) {false}
 
 
-    fun dfs(par: Int){
-        if(par == m){
+    fun bt(par : Int){
+        if(m==par) {
             println(num.joinToString(" "))
             return
         }
+        
         for(i in 1..n){
             if(!used[i]){
-                num[par] = i
-                used[i] = true
-                dfs(par+1)
-                used[i] = false
+                num[par] =  i
+                used[i]= true
+                bt(par+1)
+                used[i]=false
             }
         }
     }
-    dfs(0)
-
+    bt(0)
 }
