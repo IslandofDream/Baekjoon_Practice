e2d5f0d7a5364251ec7e3a4fcbbc96cba5006f65
IslandofDream <rhffldkt0517@naver.com>
2022-08-09 11:47:52 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1952 ms, Memory: 97076 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val cd = ArrayList<Pair<Int,Int>>(n)
+    for(i in 0 until n){
+        val (dx,dy)= readLine()!!.split(" ").map { it.toInt() }
+        cd.add(Pair(dx,dy))
+    }
+    cd.sortWith( compareBy ({it.second}, {it.first}))
+    for(i in 0 until n){
+        println("${cd[i].first} ${cd[i].second}")
+    }
+}
