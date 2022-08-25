27c6525116522d6280b33a9d96a3e9e7ad26c91a
JunwooKim <rhffldkt0517@naver.com>
2022-08-25 10:41:39 +0900

    [Silver II] Title: 최소 힙, Time: 312 ms, Memory: 28268 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -0,0 +1,19 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val sb = StringBuilder()
+    val heap = PriorityQueue<Int>()
+    repeat(readLine().toInt()) {
+        val num = readLine().toInt()
+        if (num == 0) {
+            if (heap.isNotEmpty()) sb.append("${heap.poll()}\n")
+            else sb.append("0\n")
+        } else {
+            heap.add(num)
+        }
+    }
+    print(sb)
+}
