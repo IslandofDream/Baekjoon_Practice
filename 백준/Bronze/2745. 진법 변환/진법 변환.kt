969068a8cca58504859429ce7d4ebdd1e7146668
JunwooKim <rhffldkt0517@naver.com>
2022-09-09 14:28:12 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.kt
+++ 백준/Bronze/2745. 진법 변환/진법 변환.kt
@@ -1,13 +1,15 @@
-import java.util.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
-fun main() {
-    val scan = Scanner(System.`in`)
-    var ans = 0
-    val s: String = scan.next()
-    val b: Int = scan.nextInt()
-    for (i in 0 until s.length) {
-        ans = if ('0' <= s[i] && '9' >= s[i]) ans * b + (s[i] - '0') else ans * b + (s[i] - 'A' + 10)
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (b,n) = readLine().split(" ")
+    val N = n.toInt()
+    var result = 0
+    b.forEachIndexed { index, c ->
+        if(c in '0'..'9')
+            result = result*N + (c-'0')
+        else
+            result = result*N + (c-'A'+10)
     }
-    println(ans)
+    print(result)
 }
-
