daa079bba55eb5c7832b3a9dda711e8f9553a30c
JunwooKim <rhffldkt0517@naver.com>
2022-09-13 13:22:08 +0900

    [Bronze II] Title: 하얀 칸, Time: 88 ms, Memory: 12164 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var count = 0
+    repeat(8){
+        if(it % 2 == 1) {
+            readLine().forEachIndexed { index, c ->
+                if (index % 2 == 1) {
+                    if (c == 'F') count++
+                }
+            }
+        }else{
+            readLine().forEachIndexed { index, c ->
+                if (index % 2 == 0) {
+                    if (c == 'F') count++
+                }
+            }
+        }
+    }
+    print(count)
+}
